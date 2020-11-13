3209321af72e6908ec9fc97c01f840ff5799dc87
Rudrakshi <sonirudrakshi99@gmail.com>
2020-11-14 01:15:23 +0530

    added animations

--- shared.js
+++ shared.js
@@ -4,14 +4,18 @@ var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
-
+var ctaButton = document.querySelector('main_nav__item--cta');
 for(var i=0; i < selectPlanButton.length;i++){
     selectPlanButton[i].addEventListener('click',()=>{
             //   modal.style.display = 'block';
             //   modal.style.display = 'block';
             // modal.className = 'open';
             modal.classList.add('open');
-            backdrop.classList.add('open');
+            backdrop.style.display = 'block';
+            setTimeout(function(){
+                backdrop.classList.add('open');
+            }, 10);
+           
     });
 }
 
@@ -32,15 +36,34 @@ function closeModal() {
     if(modal){
         modal.classList.remove('open');
     }
-   
     backdrop.classList.remove('open');
+    
+    setTimeout(function(){
+        backdrop.style.display = 'none';
+    }, 200);
+   
 }
 
 toggleButton.addEventListener("click",()=>{
   
-    // backdrop.style.display='block';
+     backdrop.style.display='block';
     
-    backdrop.classList.add('open');
+ 
+    setTimeout(function(){
+        backdrop.classList.add('open');
+    }, 10);
     mobileNav.classList.add('open');
     //mobileNav.style.display = 'block';
 });
+
+ctaButton.addEventListener('animationstart', (e)=> {
+    console.log('animation started',e);
+})
+
+ctaButton.addEventListener('animationend', (e)=> {
+    console.log('animation end',e);
+})
+
+ctaButton.addEventListener('animationiteration', (e)=> {
+    console.log('animation iteration',e);
+})
